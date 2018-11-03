22837a49929467a425a2149e7a94d6f99d83cba4
Delmonte_JY <39023756+KimJiYeong@users.noreply.github.com>
2018-11-03 21:09:11 +0900

    시간에 따라 단추 올라가기 구현함

--- Project/Make/coding/ui_gague.py
+++ Project/Make/coding/ui_gague.py
@@ -3,10 +3,15 @@ from pico2d import *
 class UI_gague:
     def __init__(self):
         #size = 87 658
-        self.image = load_image('Dream_gauge.png')
+        self.image_gauge = load_image('Dream_gauge.png')
+        self.image_button = load_image('Dream_Button.png')
+        self.button_y = 100
 
     def update(self):
+        self.button_y = (self.button_y + 2)
+        self.button_y = clamp(100  , self.button_y , 658)
         pass
 
     def draw(self):
-        self.image.draw(87 + 950, 1000 - 658 + 100)
+        self.image_gauge.draw(87 + 950, 1000 - 658 + 100)
+        self.image_button.draw(87 + 950, self.button_y)
